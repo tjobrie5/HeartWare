afc5cd7e2f86b865d0e6d0b1c2542017dd29ad7c
zmertens <zmertens@asu.edu>
2015-01-22 10:44:42 -0700

    modified graph

--- HeartWareness/mobile/src/main/java/heartware/wikispaces/asu/edu/heartwareness/MainActivity.java
+++ HeartWareness/mobile/src/main/java/heartware/wikispaces/asu/edu/heartwareness/MainActivity.java
@@ -58,15 +58,15 @@ public class MainActivity extends ListActivity {
         }
 
         // @TESTING : testing graph capabilities
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graph.addSeries(series);
+//        GraphView graph = (GraphView) findViewById(R.id.graph);
+//        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+//                new DataPoint(0, 1),
+//                new DataPoint(1, 5),
+//                new DataPoint(2, 3),
+//                new DataPoint(3, 2),
+//                new DataPoint(4, 6)
+//        });
+//        graph.addSeries(series);
 
         Log.d(TAG, " == onCreate() == ");
     }
