777ec2b132db5a99b3ab9a8c85d2d59bc0101314
zmertens <zmertens@asu.edu>
2015-03-14 15:51:32 -0700

    created master app

---
+++ Heartware_master/app/src/main/java/heartware/com/heartware_master/MainActivity.java
@@ -0,0 +1,62 @@
+package heartware.com.heartware_master;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.facebook.AppEventsLogger;
+import com.facebook.Session;
+import com.jawbone.upplatformsdk.api.ApiManager;
+
+public class MainActivity extends ActionBarActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        // Logs 'install' and 'app activate' App Events
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
