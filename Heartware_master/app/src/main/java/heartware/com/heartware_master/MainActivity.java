cdecdfe7a2b87a213b4c42dc0564d53be522cc7c
zmertens <zmertens@asu.edu>
2015-03-16 13:35:06 -0700

    added new dialog class, some FB features

--- Heartware_master/app/src/main/java/heartware/com/heartware_master/MainActivity.java
+++ Heartware_master/app/src/main/java/heartware/com/heartware_master/MainActivity.java
@@ -21,7 +21,14 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.facebook.AppEventsLogger;
+import com.facebook.FacebookAuthorizationException;
+import com.facebook.FacebookOperationCanceledException;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
+import com.facebook.widget.ProfilePictureView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -39,17 +46,45 @@ public class MainActivity extends Activity implements View.OnClickListener
     private EditText etDisabilities;
     private EditText etWorkoutLocations;
     private DBAdapter dbAdapter;
+    // Facebook stuff
     private LoginButton bAuthButton;
+    private GraphUser mUser;
+    private ProfilePictureView mProfilePictureView;
+    private enum PendingAction { NONE, POST_PHOTO, POST_STATUS_UPDATE };
+    private UiLifecycleHelper mUIHelper;
+    private PendingAction mPendingAction = PendingAction.NONE;
+
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mUIHelper = new UiLifecycleHelper(this, callback);
+        mUIHelper.onCreate(savedInstanceState);
+
+
         dbAdapter = new DBAdapter(this);
 
         bAuthButton = (LoginButton) findViewById(R.id.bAuthButton);
-        bAuthButton.setOnClickListener(this);
+        bAuthButton.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+            @Override
+            public void onUserInfoFetched(GraphUser graphUser) {
+                Log.d(TAG, "in onUserInfoFetched - bAuthButton");
+                // save the user
+                mUser = graphUser;
+                updateUI();
+                handlePendingAction();
+            }
+        });
+
+        mProfilePictureView = (ProfilePictureView) findViewById(R.id.vProfilePicture);
 
         b_graphs = (Button) findViewById(R.id.bGoals);
         b_graphs.setOnClickListener(this);
@@ -92,6 +127,7 @@ public class MainActivity extends Activity implements View.OnClickListener
                 break;
 
             case R.id.bFriends:
+                // @TODO : verify that the user is logged into Facebook first (mUser != null)
                 startActivity(new Intent(getApplicationContext(), FriendsActivity.class));
                 break;
 
@@ -115,7 +151,8 @@ public class MainActivity extends Activity implements View.OnClickListener
                 else {
                     // perform update to database
                     HashMap<String, String> queryValues = new HashMap<>();
-                    queryValues.put(DBAdapter.PROFILE_ID, new String("0")); // only 1 profile
+                    // Profile_ID is hardcoded - only 1 profile
+                    queryValues.put(DBAdapter.PROFILE_ID, new String("1"));
                     queryValues.put(DBAdapter.USER_NAME, etUserName.getText().toString());
                     queryValues.put(DBAdapter.SEX, etSex.getText().toString());
                     queryValues.put(DBAdapter.FAV_EXERCISE, etExercises.getText().toString());
@@ -124,7 +161,6 @@ public class MainActivity extends Activity implements View.OnClickListener
                     dbAdapter.updateProfile(queryValues);
                 }
                 break;
-
             default:
                 break;
         }
@@ -135,15 +171,89 @@ public class MainActivity extends Activity implements View.OnClickListener
     protected void onResume()
     {
         super.onResume();
+        mUIHelper.onResume();
         // Logs 'install' and 'app activate' App Events
         AppEventsLogger.activateApp(this);
+        updateUI();
     }
 
     @Override
     protected void onPause()
     {
         super.onPause();
+        mUIHelper.onPause();
         // Logs 'app deactivate' App Event.
         AppEventsLogger.deactivateApp(this);
     }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        mUIHelper.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        mUIHelper.onSaveInstanceState(outState);
+
+//        outState.putString(PENDING_ACTION_BUNDLE_KEY, pendingAction.name());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+//        uiHelper.onActivityResult(requestCode, resultCode, data, dialogCallback);
+    }
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (mPendingAction != PendingAction.NONE &&
+                (exception instanceof FacebookOperationCanceledException ||
+                        exception instanceof FacebookAuthorizationException)) {
+//            new AlertDialog.Builder(HelloFacebookSampleActivity.this)
+//                    .setTitle(R.string.cancelled)
+//                    .setMessage(R.string.permission_not_granted)
+//                    .setPositiveButton(R.string.ok, null)
+//                    .show();
+            mPendingAction = PendingAction.NONE;
+        } else if (state == SessionState.OPENED_TOKEN_UPDATED) {
+            handlePendingAction();
+        }
+        updateUI();
+    }
+
+    // UI updates after user logins in
+    private void updateUI()
+    {
+        Session session = Session.getActiveSession();
+        boolean enableButtons = (session != null && session.isOpened());
+
+        if(enableButtons && mUser != null) {
+            mProfilePictureView.setProfileId(mUser.getId());
+        }
+        else {
+            mProfilePictureView.setProfileId(null);
+        }
+    }
+
+    @SuppressWarnings("incomplete-switch")
+    private void handlePendingAction() {
+        PendingAction previouslyPendingAction = mPendingAction;
+        // These actions may re-set pendingAction if they are still pending, but we assume they
+        // will succeed.
+        mPendingAction = PendingAction.NONE;
+
+        switch (previouslyPendingAction) {
+            case POST_PHOTO:
+//                postPhoto();
+                break;
+            case POST_STATUS_UPDATE:
+//                postStatusUpdate();
+                break;
+        }
+    }
+
 } // MainActivity class
