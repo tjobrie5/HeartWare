2e7ce182672f73c9e710ea77ce050fef65990e8d
zmertens <zmertens@asu.edu>
2015-04-04 14:54:12 -0700

    started to add Tabs

--- Heartware_master/app/src/main/java/heartware/com/heartware_master/MainActivity.java
+++ Heartware_master/app/src/main/java/heartware/com/heartware_master/MainActivity.java
@@ -23,6 +23,8 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -31,7 +33,6 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.jawbone.upplatformsdk.utils.UpPlatformSdkConstants;
