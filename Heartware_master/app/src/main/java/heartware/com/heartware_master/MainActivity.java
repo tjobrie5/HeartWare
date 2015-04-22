280c02e84fbbede9087583ae09884246e5f0c712
tjobrie5 <tjobrie5@asu.edu>
2015-04-21 22:38:33 -0700

    User Calendar info and notification

--- Heartware_master/app/src/main/java/heartware/com/heartware_master/MainActivity.java
+++ Heartware_master/app/src/main/java/heartware/com/heartware_master/MainActivity.java
@@ -15,30 +15,42 @@
 
 package heartware.com.heartware_master;
 
+import android.app.ActionBar;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.CalendarContract;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-
-import android.app.ActionBar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.facebook.appevents.AppEventsLogger;
-import com.jawbone.upplatformsdk.api.ApiManager;
 import com.jawbone.upplatformsdk.utils.UpPlatformSdkConstants;
 
-import java.util.HashMap;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends FragmentActivity implements LoginDialogFragment.LoginDialogListener,
         ProfileDialogFragment.ProfileDialogListener, MeetupDialogFragment.MeetupDialogListener
@@ -52,6 +64,11 @@ public class MainActivity extends FragmentActivity implements LoginDialogFragmen
     private DBAdapter mDBAdapter;
     private LoginDialogFragment mLoginDialog;
     private JawboneUpHelper mJboneHelper;
+
+    private boolean notification = false;
+    private NotificationManager notificationManager;
+    private int noteID = 100;
+    private String avail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -68,12 +85,39 @@ public class MainActivity extends FragmentActivity implements LoginDialogFragmen
         mLoginDialog.show(getFragmentManager(), TAG);
 
         setupActionTabs();
+
+        //get user calendar availability
+        avail = checkEvents();
+        Toast.makeText(this, "Available: " + avail, Toast.LENGTH_LONG).show();
+
+        //send user availability to server
+        CalToServer calToServer = (CalToServer) new CalToServer().execute(new String(avail));
+
     } // onCreate
 
     @Override
     public void onResume()
     {
         super.onResume();
+
+        if(!notification)
+        {
+            //prepare intent that is triggered when notification is selected
+            Intent intent = new Intent(this, MeetupsFragment.class);
+            PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            //build notification
+            Notification n = new Notification.Builder(this).setContentTitle("Heartware").setContentText("Reminder: You have a Meetup today!").setContentIntent(pIntent).setSmallIcon(R.mipmap.ic_launcher).build();
+
+            notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+            notificationManager.notify(noteID,n);
+
+
+            //HeartwareApplication app = (HeartwareApplication) this.getApplication();
+            //final ArrayList<HashMap<String, String>> meetups = mDBAdapter.getAllMeetups(app.getCurrentProfileId());
+
+        }
 
         // Call the 'activateApp' method to log an app event for use in analytics and advertising
         // reporting.  Do so in the onResume methods of the primary Activities that an app may be
@@ -255,6 +299,170 @@ public class MainActivity extends FragmentActivity implements LoginDialogFragmen
     {
 
     }
+
+    public String checkEvents(){
+        //boolean variables for morning, afternoon, and evening
+        boolean mB = true, aB = true, eB = true;
+
+        //string for result of calendar query available time
+        String result;
+
+        String[] INSTANCE_PROJECTION = new String[]{
+                CalendarContract.Events.CALENDAR_ID,
+                CalendarContract.Events.TITLE,
+                CalendarContract.Events.DESCRIPTION,
+                CalendarContract.Events.DTSTART,
+                CalendarContract.Events.DTEND,
+                CalendarContract.Events.ALL_DAY,
+                CalendarContract.Events.EVENT_LOCATION
+        };
+
+        //get current time and date
+        Calendar c = Calendar.getInstance();
+        int second = c.get(Calendar.SECOND);
+        int minute = c.get(Calendar.MINUTE);
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int month = c.get(Calendar.MONTH);
+        int year = c.get(Calendar.YEAR);
+
+        long startMillis, endMillis;
+
+        //variables for if time of day is being evaluated
+        boolean morningE = false, afternoonE = false, eveningE = false;
+
+        //only evaluate times if they are in the future
+        if(hour < 12) {
+            morningE = true;
+            afternoonE = true;
+            eveningE = true;
+        }
+        else if ( hour >= 12 && hour < 17)
+        {
+            morningE = false;
+            afternoonE = true;
+            eveningE = true;
+        }
+        else
+        {
+            morningE = false;
+            afternoonE = false;
+            eveningE = true;
+        }
+
+        //check if times of day are free and set their flags to true or false
+        if(morningE)
+        {
+            c.set(year, month, day, hour, minute, second);
+            startMillis = c.getTimeInMillis();
+
+            //set end time to noon
+            Calendar mC = Calendar.getInstance();
+            mC.set(year, month, day, 11, 59, 59);
+            endMillis = mC.getTimeInMillis();
+
+            String selection = "(( " + CalendarContract.Events.DTSTART + " >= " + startMillis + ") AND ( " + CalendarContract.Events.DTSTART + " <= " + endMillis + " ))";
+            Cursor cursor = this.getBaseContext().getContentResolver().query( CalendarContract.Events.CONTENT_URI, INSTANCE_PROJECTION, selection, null, null );
+
+            if(cursor.moveToFirst()) {
+                mB = false;
+            }
+            else
+                mB = true;
+        }
+
+        if(afternoonE)
+        {
+            //set begin time to noon
+            c.set(year, month, day, 12, 0, 0);
+            startMillis = c.getTimeInMillis();
+
+            //set end time to 5pm
+            Calendar aC = Calendar.getInstance();
+            aC.set(year, month, day, 16, 59, 59);
+            endMillis = aC.getTimeInMillis();
+
+            String selection = "(( " + CalendarContract.Events.DTSTART + " >= " + startMillis + ") AND ( " + CalendarContract.Events.DTSTART + " <= " + endMillis + " ))";
+            Cursor cursor = this.getBaseContext().getContentResolver().query( CalendarContract.Events.CONTENT_URI, INSTANCE_PROJECTION, selection, null, null );
+
+            if(cursor.moveToFirst()) {
+                aB = false;
+            }
+            else
+                aB = true;
+        }
+
+        if(eveningE)
+        {
+            //set begin time to 5pm
+            c.set(year, month, day, 17, 0, 0);
+            startMillis = c.getTimeInMillis();
+
+            //set end time to 11pm
+            Calendar eC = Calendar.getInstance();
+            eC.set(year, month, day, 22, 59, 59);
+            endMillis = eC.getTimeInMillis();
+
+            String selection = "(( " + CalendarContract.Events.DTSTART + " >= " + startMillis + ") AND ( " + CalendarContract.Events.DTSTART + " <= " + endMillis + " ))";
+            Cursor cursor = this.getBaseContext().getContentResolver().query( CalendarContract.Events.CONTENT_URI, INSTANCE_PROJECTION, selection, null, null );
+
+            if(cursor.moveToFirst()) {
+                eB = false;
+            }
+            else
+                eB = true;
+        }
+
+        //return either morning, afternoon, evening, or none for time free on calendar for the day
+        if(morningE && mB)
+            result = "morning";
+        else if(afternoonE && aB)
+            result = "afternoon";
+        else if(eveningE && eB)
+            result = "evening";
+        else
+            result = "none";
+
+        return result;
+    }
+
+    private class CalToServer extends AsyncTask<String, Void, String>
+    {
+        private static final String URL = "http://qqroute.com:8080/sendCal";
+        @Override
+        protected String doInBackground(String... params)
+        {
+            try {
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(URL);
+                List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(2);
+                nameValuePair.add(new BasicNameValuePair("cal", avail));
+                try {
+                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+                }
+                catch(UnsupportedEncodingException ex) {
+                    Log.d(TAG, ex.getMessage().toString());
+                }
+
+                try {
+                    HttpResponse res = httpClient.execute(httpPost);
+                    Log.d(TAG, "Http Post Response: " + res.toString());
+                }
+                catch(ClientProtocolException ex) {
+                    Log.d(TAG, ex.getMessage().toString());
+                }
+                catch(IOException ex) {
+                    Log.d(TAG, ex.getMessage().toString());
+                }
+            }
+            catch(Exception e) {
+                Log.d(TAG, e.getMessage().toString());
+                return e.getMessage().toString();
+            }
+
+            return "doInBackground() -- TokenToServer";
+        }
+    } // TokenToServer class
 
 
 
